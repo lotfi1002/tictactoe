52971907617371974987fe465374a4aa6246f843
SmartProjects <36716111+lotfi1002@users.noreply.github.com>
2019-11-13 11:49:54 +0100

    Add files via upload

---
+++ __init__.py
@@ -0,0 +1,3 @@
+from lib.agent import Qlearning
+from lib.game import Game
+from lib.teacher import Teacher
---
+++ agent.py
@@ -0,0 +1,22 @@
+import numpy as np 
+import os
+import pickle
+import collections
+import random
+
+class Qlearning :
+    """
+     class for Q-learning .
+    Parameters
+    ----------
+    alpha : float 
+        learning rate
+    gamma : float
+        temporal discounting rate
+    eps : float 
+        probability of random action vs. greedy action
+    eps_decay : float
+        epsilon decay rate. Larger value = more decay
+    """
+    pass
+        
